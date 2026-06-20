09bc5fc38f063f28b026491121a1abbac84528a7
Hulan1234 <163529385+Hulan1234@users.noreply.github.com>
2026-06-21 01:01:05 +0800

    IVR Mod

--- src/main/java/net/hulan/ivr/client/IVRClientData.java
+++ src/main/java/net/hulan/ivr/client/IVRClientData.java
@@ -3,9 +3,4 @@ package net.hulan.ivr.client;
 public class IVRClientData {
 
     public static final IVRClientCache DATA_CACHE = new IVRClientCache();
-
-    public static void receivePacket() {
-        DATA_CACHE.sync();
-        DATA_CACHE.refreshDynamicResources();
-    }
 }
